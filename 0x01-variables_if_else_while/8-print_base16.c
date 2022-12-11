a8ce802f35d5f0a4e8410e80edc4079a73aca171
amgenious <henrytweneboah956@gmail.com>
2022-12-10 17:21:33 -0800

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putcahr((num % 10) + '0');
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+	putchar('\n');
+	return (0);
+}
