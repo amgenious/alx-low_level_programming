d3b525ad85833a45999cd7198e6259efa9f5aab5
amgenious <henrytweneboah956@gmail.com>
2022-12-10 17:14:43 -0800

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	char al;
+
+	for (al = 'z'; al >= 'a'; al--)
+		putchar(le);
+	putchar('\n');
+	return (0);
+}
