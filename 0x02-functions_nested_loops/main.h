fa0e303817835dd0962f5998e39315d505d9dbc7
amgenious <henrytweneboah956@gmail.com>
2022-12-13 17:33:17 -0800

    task1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 void print_alphabet(void);
-voaid print_alphabet_x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -12,6 +12,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(chat c);
+int _putchar(char c);
 
 #endif
